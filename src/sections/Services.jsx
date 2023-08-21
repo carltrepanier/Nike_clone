a6dea5cb67a10915d0a489de401c083c0e7a0f33
At0micTurtle <carltrep@videotron.ca>
2023-08-21 16:22:07 -0400

    first commit

---
+++ src/sections/Services.jsx
@@ -0,0 +1,12 @@
+import { services } from "../constants/index";
+import { ServiceCard } from "../components/index";
+
+export default function Services() {
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  )
+}
